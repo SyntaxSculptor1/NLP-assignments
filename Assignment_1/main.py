1b2d882a1160dbdbb736c24bce2dd9be3444d7f4
Toshiro-kyun <discordtoshiro@gmail.com>
2026-02-17 19:39:09 +0100

    Added post tokenenization normalization and logisitic regression

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV, PredefinedSplit
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import MaxAbsScaler
 from datasets import load_dataset
 from typing import Iterator, List
 import nltk
@@ -8,7 +10,12 @@ from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 import re
 import html
+import string
+from scipy.sparse import csr_matrix, vstack
+import numpy as np
 
+
+nltk.download("wordnet")
 nltk.download("punkt_tab")
 
 SEED = 67
@@ -17,8 +24,6 @@ SPLIT = 0.1
 #! THIS DOES NOT WORK AT ALL BUT THE IDEA IS SIMILAR TO THIS
 LEMMATIZER = WordNetLemmatizer()
 VECTORIZER = TfidfVectorizer(
-    tokenizer=LEMMATIZER,
-    lowercase=True,
     stop_words="english",
     token_pattern=r"(?u)\b\w\w+\b",
 )
@@ -90,15 +95,82 @@ def tokenize_inplace(datasets: List[pd.DataFrame]):
 # 2. lemmatisation
 # 3. Stop word removal
 # 4. maybe number removal?
-def normalization_helper(text: str):
+def post_tokenize_normalize_helper(text: List[str]) -> List[str]:
+    cleaned_text = []
 
-    return text
+    for word in text:
+        word = word.strip(string.punctuation)
 
+        if not word:
+            continue
 
-def normalize_inplace(datasets: List[pd.DataFrame]):
-    # DOES NOT WORK
+        word = word.lower()
+        word_lemma = LEMMATIZER.lemmatize(word)
+
+        cleaned_text.append(word_lemma)
+    
+    return " ".join(cleaned_text)
+
+def post_tokenize_normalize_inplace(datasets: List[pd.DataFrame]):
     for dataset in datasets:
-        dataset["features"] = VECTORIZER.fit_transform(dataset["features"])
+        dataset["features"] = dataset["features"].apply(post_tokenize_normalize_helper)
+
+def tfidf_transform(datasets: List[pd.DataFrame]) -> List[pd.DataFrame]:
+    x_values = []
+    for idx, dataset in enumerate(datasets):
+        if idx == 0:
+            features = VECTORIZER.fit_transform(dataset["features"])
+        else:
+            features = VECTORIZER.transform(dataset["features"])
+
+        x_values.append(features)
+
+    return x_values
+
+def logistic_regression(
+    x_train: csr_matrix,
+    x_val: csr_matrix,
+    x_test: csr_matrix,
+    y_train: pd.Series,
+    y_val: pd.Series,
+    y_test: pd.Series
+) -> None:
+
+    scaler = MaxAbsScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_val_scaled = scaler.transform(x_val)
+    x_test_scaled = scaler.transform(x_test)
+
+    x_full = vstack([x_train_scaled, x_val_scaled])
+    y_full = np.concatenate([y_train, y_val])
+
+    train_indices = np.full(x_train.shape[0], -1)
+    val_indices = np.full(x_val.shape[0], 0)
+
+    test_fold = np.concatenate([train_indices, val_indices])
+
+    ps = PredefinedSplit(test_fold)
+
+    param_grid = {
+        'C': [0.001, 0.01, 0.1, 1, 10, 100],
+        'penalty': ['l1', 'l2']
+    }
+
+    grid_search = GridSearchCV(
+        estimator=LogisticRegression(max_iter=5000),
+        param_grid=param_grid,
+        cv=ps, 
+        scoring='accuracy',
+        n_jobs=-1,
+        verbose=1
+    )
+
+    grid_search.fit(x_full, y_full)
+
+    print(f"Best C found: {grid_search.best_params_['C']}")
+    print(f"Validation Accuracy: {grid_search.best_score_}")
+
+    return grid_search.best_estimator_
 
 
 def main():
@@ -112,12 +184,21 @@ def main():
 
     tokenize_inplace([train_data, validation_data, test_data])
 
-    # CURRENTLY WORKS UP TO HERE, NORMALISATION DOES NOT WORK YET
-    normalize_inplace([train_data, validation_data, test_data])
+    post_tokenize_normalize_inplace([train_data, validation_data, test_data])
+
+    x_train, x_val, x_test = tfidf_transform([train_data, validation_data, test_data])
+    y_train, y_val, y_test = train_data["label"], validation_data["label"], test_data["label"]
+
+    logistic_regression(
+        x_train,
+        x_val,
+        x_test,
+        y_train,
+        y_val,
+        y_test
+    )
 
     train_data.to_csv("ag_news_processed.csv", index=False)
-
-    print("We are done")
 
 
 if __name__ == "__main__":
