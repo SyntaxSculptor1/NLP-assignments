682a44e318709dca93b0d291f5cf4c33e4581abd
Toshiro-kyun <discordtoshiro@gmail.com>
2026-02-17 23:47:00 +0100

    Assignment 1 - Code cleaning and implementation of logisic regression and support vector machine and their evaluations

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -1,48 +1,50 @@
+import string
+import re
+import html
+
+import nltk
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV, PredefinedSplit
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, f1_score, confusion_matrix
+from sklearn.svm import SVC
 from sklearn.preprocessing import MaxAbsScaler
 from datasets import load_dataset
-from typing import Iterator, List
-import nltk
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-import re
-import html
-import string
-from scipy.sparse import csr_matrix, vstack
+
 import numpy as np
+from scipy import sparse
+
+from typing import List, Iterator, Tuple
+import argparse
 
 
-nltk.download("wordnet")
-nltk.download("punkt_tab")
+def load_nltk_models() -> None:
+    nltk.download("wordnet")
+    nltk.download("punkt_tab")
 
-SEED = 67
-SPLIT = 0.1
+def load_data(
+    dataset: str = "sh0416/ag_news",
+    split: float = 0.1,
+    seed: int = 67
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    
+    full_train_data = load_dataset(dataset, split="train").to_pandas()
 
-#! THIS DOES NOT WORK AT ALL BUT THE IDEA IS SIMILAR TO THIS
-LEMMATIZER = WordNetLemmatizer()
-VECTORIZER = TfidfVectorizer(
-    stop_words="english",
-    token_pattern=r"(?u)\b\w\w+\b",
-)
-
-
-def load_data():
-    full_train_data = load_dataset("sh0416/ag_news", split="train").to_pandas()
     train_data, validation_data = train_test_split(
-        full_train_data, random_state=SEED, test_size=SPLIT, shuffle=True
+        full_train_data, random_state=seed, test_size=split, shuffle=True
     )
 
-    test_data = load_dataset("sh0416/ag_news", split="test").to_pandas()
+    test_data = load_dataset(dataset, split="test").to_pandas()
 
-    if (
-        isinstance(train_data, Iterator)
-        or isinstance(validation_data, Iterator)
-        or isinstance(test_data, Iterator)
-    ):
-        raise Exception("One of the datasets is an Iterator object")
+    assert (
+        not isinstance(train_data, Iterator) and
+        not isinstance(validation_data, Iterator) and
+        not isinstance(test_data, Iterator)
+    ) 
 
     return (
         pd.DataFrame(train_data),
@@ -51,51 +53,37 @@ def load_data():
     )
 
 
-def merge_inplace_columns(datasets: List[pd.DataFrame]):
-    """
-    Inplace merge of title and description
-    """
+def merge_title_description(
+    datasets: List[pd.DataFrame],
+    title_column: str = "title",
+    description_column: str = "description",
+    new_column: str = "text"
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    
+    datasets = tuple(dataset.copy(deep=True) for dataset in datasets)
+
     for dataset in datasets:
-        dataset["features"] = (
-            dataset["title"].astype(str) + " " + dataset["description"].astype(str)
+        dataset[new_column] = (
+            dataset[title_column].astype(str) + " " + dataset[description_column].astype(str)
         )
-        dataset.drop(["title", "description"], axis=1, inplace=True)
+        dataset.drop([title_column, description_column], axis=1, inplace=True)
+    
+    return datasets
 
-
-def pre_tokenize_clean_helper(text: str):
-    """
-    Helper function that tells us what to clean up when tokenizing
-    """
-    # convert HTML entities into < and >
+def pre_tokenization_normalization_helper(
+    text: str
+) -> str:
     text = html.unescape(text)
-    # remove entire HTML tags between < >
     clean_text = re.sub(r"<[^>]+>", "", text)
-    # clean up white spaces
     clean_text = re.sub(r"\s+", " ", clean_text).strip()
-
+    clean_text = clean_text.lower()
     return clean_text
-
-
-def pre_tokenization_normalisation_inplace(datasets: List[pd.DataFrame]):
-    """
-    Normalisation done before tokenisation in place
-    """
-
-    for dataset in datasets:
-        dataset["features"] = dataset["features"].apply(pre_tokenize_clean_helper)
-
-
-def tokenize_inplace(datasets: List[pd.DataFrame]):
-    for dataset in datasets:
-        dataset["features"] = dataset["features"].apply(word_tokenize)
-
-
-##! NORMALISATION NEEDS THE FOLLOWING:
-# 1. lower case
-# 2. lemmatisation
-# 3. Stop word removal
-# 4. maybe number removal?
-def post_tokenize_normalize_helper(text: List[str]) -> List[str]:
+    
+def post_tokenization_normalization_helper(
+    text: List[str],
+    lemmatizer: WordNetLemmatizer
+) -> str:
+    
     cleaned_text = []
 
     for word in text:
@@ -104,101 +92,265 @@ def post_tokenize_normalize_helper(text: List[str]) -> List[str]:
         if not word:
             continue
 
-        word = word.lower()
-        word_lemma = LEMMATIZER.lemmatize(word)
+        word_lemma = lemmatizer.lemmatize(word)
 
         cleaned_text.append(word_lemma)
     
     return " ".join(cleaned_text)
 
-def post_tokenize_normalize_inplace(datasets: List[pd.DataFrame]):
+def text_cleaning(
+    datasets: List[pd.DataFrame],
+    lemmatizer: WordNetLemmatizer,
+    column: str = "text",
+) -> Tuple[pd.DataFrame, pd.DataFrame, pd.DataFrame]:
+    datasets = tuple(dataset.copy(deep=True) for dataset in datasets)
+
     for dataset in datasets:
-        dataset["features"] = dataset["features"].apply(post_tokenize_normalize_helper)
+        dataset[column] = dataset[column].apply(pre_tokenization_normalization_helper)
+        dataset[column] = dataset[column].apply(word_tokenize)
+        dataset[column] = dataset[column].apply(post_tokenization_normalization_helper, lemmatizer=lemmatizer)
 
-def tfidf_transform(datasets: List[pd.DataFrame]) -> List[pd.DataFrame]:
-    x_values = []
-    for idx, dataset in enumerate(datasets):
+    return datasets
+
+def tfidf_transform(
+    train_texts: pd.Series,
+    validation_texts: pd.Series,
+    test_texts: pd.Series,
+    vectorizer: TfidfVectorizer
+) -> List[sparse.csr_matrix]:
+    features = []
+    for idx, dataset in enumerate([train_texts, validation_texts, test_texts]):
         if idx == 0:
-            features = VECTORIZER.fit_transform(dataset["features"])
+            text_features = vectorizer.fit_transform(dataset)
         else:
-            features = VECTORIZER.transform(dataset["features"])
+            text_features = vectorizer.transform(dataset)
 
-        x_values.append(features)
+        features.append(text_features)
 
-    return x_values
+    return features
 
-def logistic_regression(
-    x_train: csr_matrix,
-    x_val: csr_matrix,
-    x_test: csr_matrix,
+def perform_logistic_regression(
+    x_train: sparse.csr_matrix,
+    x_val: sparse.csr_matrix,
     y_train: pd.Series,
     y_val: pd.Series,
-    y_test: pd.Series
-) -> None:
+    param_grid: dict,
+    scoring: str = "accuracy",
+    max_iter: int = 5000,
+    verbose: int = 1,
+    seed: int = 67
+) -> GridSearchCV:
 
     scaler = MaxAbsScaler()
     x_train_scaled = scaler.fit_transform(x_train)
     x_val_scaled = scaler.transform(x_val)
-    x_test_scaled = scaler.transform(x_test)
 
-    x_full = vstack([x_train_scaled, x_val_scaled])
+    x_full = sparse.vstack([x_train_scaled, x_val_scaled])
     y_full = np.concatenate([y_train, y_val])
 
     train_indices = np.full(x_train.shape[0], -1)
     val_indices = np.full(x_val.shape[0], 0)
 
-    test_fold = np.concatenate([train_indices, val_indices])
+    split_indices = np.concatenate([train_indices, val_indices])
 
-    ps = PredefinedSplit(test_fold)
-
-    param_grid = {
-        'C': [0.001, 0.01, 0.1, 1, 10, 100],
-        'penalty': ['l1', 'l2']
-    }
+    predefined_split = PredefinedSplit(split_indices)
 
     grid_search = GridSearchCV(
-        estimator=LogisticRegression(max_iter=5000),
+        estimator=LogisticRegression(max_iter=max_iter, random_state=seed),
         param_grid=param_grid,
-        cv=ps, 
-        scoring='accuracy',
+        cv=predefined_split, 
+        scoring=scoring,
+        verbose=verbose,
         n_jobs=-1,
-        verbose=1
     )
 
     grid_search.fit(x_full, y_full)
 
-    print(f"Best C found: {grid_search.best_params_['C']}")
-    print(f"Validation Accuracy: {grid_search.best_score_}")
+    return grid_search
 
-    return grid_search.best_estimator_
+def perform_support_vector_machine(
+    x_train: sparse.csr_matrix,
+    x_val: sparse.csr_matrix,
+    y_train: pd.Series,
+    y_val: pd.Series,
+    param_grid: dict,
+    scoring: str = "accuracy",
+    verbose: int = 1
+) -> GridSearchCV:
+    
+    scaler = MaxAbsScaler()
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_val_scaled = scaler.transform(x_val)
 
+    x_full = sparse.vstack([x_train_scaled, x_val_scaled])
+    y_full = np.concatenate([y_train, y_val])
 
-def main():
-    train_data, validation_data, test_data = load_data()
+    train_indices = np.full(x_train.shape[0], -1)
+    val_indices = np.full(x_val.shape[0], 0)
 
-    train_data.to_csv("ag_news_raw.csv", index=False)
+    split_indices = np.concatenate([train_indices, val_indices])
 
-    merge_inplace_columns([train_data, validation_data, test_data])
+    predefined_split = PredefinedSplit(split_indices)
 
-    pre_tokenization_normalisation_inplace([train_data, validation_data, test_data])
-
-    tokenize_inplace([train_data, validation_data, test_data])
-
-    post_tokenize_normalize_inplace([train_data, validation_data, test_data])
-
-    x_train, x_val, x_test = tfidf_transform([train_data, validation_data, test_data])
-    y_train, y_val, y_test = train_data["label"], validation_data["label"], test_data["label"]
-
-    logistic_regression(
-        x_train,
-        x_val,
-        x_test,
-        y_train,
-        y_val,
-        y_test
+    grid_search = GridSearchCV(
+        estimator=SVC(),
+        param_grid=param_grid,
+        cv=predefined_split, 
+        scoring=scoring,
+        verbose=verbose,
+        n_jobs=-1,
     )
 
-    train_data.to_csv("ag_news_processed.csv", index=False)
+    grid_search.fit(x_full, y_full)
+
+    return grid_search
+
+def evaluate_logistic_regression(
+    model: LogisticRegression,
+    x_test: sparse.csr_matrix,
+    y_test: pd.Series
+) -> None:
+    y_pred = model.predict(x_test)
+
+    print(f"Model - Logistic Regression (C = {model.get_params()['C']})")
+    print("Accuracy:", accuracy_score(y_true= y_test, y_pred= y_pred))
+    print("F1 Score:", f1_score(y_true=y_test, y_pred=y_pred, average="macro"))
+
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_true= y_test, y_pred= y_pred))
+
+def evaluate_support_vector_machine(
+    model: SVC,
+    x_test: sparse.csr_matrix,
+    y_test: pd.Series
+) -> None:
+    y_pred = model.predict(x_test)
+
+    print(f"Model - Support Vector Machine (C = {model.get_params()['C']})")
+    print("Accuracy:", accuracy_score(y_true= y_test, y_pred= y_pred))
+    print("F1 Score:", f1_score(y_true=y_test, y_pred=y_pred, average="macro"))
+
+    print("Confusion Matrix:")
+    print(confusion_matrix(y_true= y_test, y_pred= y_pred))
+
+def argument_parsing() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--dataset",
+        default="sh0416/ag_news",
+        type=str,
+        help="Dataset to use. Default: sh0416/ag_news"
+    )
+
+    parser.add_argument(
+        "--verbose",
+        default=True,
+        type=bool,
+        help="Verbose mode. Default: True"
+    )
+
+    parser.add_argument(
+        "--split",
+        default=0.1,
+        type=float,
+        help="Split ratio. Default: 0.1"
+    )
+
+    parser.add_argument(
+        "--seed",
+        default=67,
+        type=int,
+        help="Random seed. Default: 67"
+    )
+
+    args = parser.parse_args()
+
+    return args
+
+def main() -> None:
+    args = argument_parsing()
+
+    DATASET = args.dataset
+    VERBOSE = args.verbose
+    SPLIT = args.split
+    SEED = args.seed
+
+    load_nltk_models()
+
+    train_data, validation_data, test_data = load_data(
+        dataset=DATASET,
+        split=SPLIT,
+        seed=SEED
+    )
+
+    train_data, validation_data, test_data = merge_title_description(
+        datasets=[train_data, validation_data, test_data],
+        title_column="title",
+        description_column="description",
+        new_column="text"
+    )
+
+    lemmatizer = WordNetLemmatizer()
+    train_data, validation_data, test_data = text_cleaning(
+        datasets=[train_data, validation_data, test_data],
+        column="text",
+        lemmatizer = lemmatizer
+    )
+
+    vectorizer = TfidfVectorizer(
+        lowercase=True,
+        stop_words="english"
+    )
+
+    x_train, x_val, x_test = tfidf_transform(
+        train_texts= train_data["text"],
+        validation_texts= validation_data["text"],
+        test_texts= test_data["text"],
+        vectorizer= vectorizer
+    )
+
+    y_train, y_val, y_test = train_data["label"], validation_data["label"], test_data["label"]
+
+    logreg_param_grid = {
+        "C": [0.001, 0.01, 0.1, 1, 10, 100]
+    }
+    logreg_grid_search = perform_logistic_regression(
+        x_train=x_train,
+        x_val=x_val,
+        y_train=y_train,
+        y_val=y_val,
+        param_grid=logreg_param_grid,
+        scoring="accuracy",
+        max_iter=5000,
+        verbose=int(VERBOSE),
+        seed=SEED
+    )
+
+    evaluate_logistic_regression(
+        model=logreg_grid_search.best_estimator_,
+        x_test=x_test,
+        y_test=y_test
+    )
+
+    svm_param_grid = {
+        "C": [0.001, 0.01, 0.1, 1, 10, 100]
+    }
+    svm_grid_search = perform_support_vector_machine(
+        x_train=x_train,
+        x_val=x_val,
+        y_train=y_train,
+        y_val=y_val,
+        param_grid=svm_param_grid,
+        scoring="accuracy",
+        verbose=int(VERBOSE)
+    )
+
+    evaluate_support_vector_machine(
+        model=svm_grid_search.best_estimator_,
+        x_test=x_test,
+        y_test=y_test
+    )
 
 
 if __name__ == "__main__":
